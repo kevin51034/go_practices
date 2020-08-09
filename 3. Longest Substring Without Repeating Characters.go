62f25f6b98f2fe4df4c5795632144760dd343c13
陳文擎 <kevin51034@gmail.com>
2020-08-09 14:59:40 +0800

    2020/08/09

--- 3. Longest Substring Without Repeating Characters.go
+++ 3. Longest Substring Without Repeating Characters.go
@@ -25,3 +25,26 @@ func max(x, y int) int {
     }
     return y
 }
+
+// optimize
+
+func lengthOfLongestSubstring(s string) int {
+    index := [128]int{}
+    ans := 0
+    for i:=0; i<128; i++ {
+        index[i] = -1
+    }
+    for i,j:=0,0; j<len(s); j++ {
+        i = max(i, index[s[j]]+1)
+        ans = max(ans, j-i+1)
+        index[s[j]] = j
+    }
+    return ans
+}
+
+func max(x, y int) int {
+    if x>y {
+        return x
+    }
+    return y
+}
