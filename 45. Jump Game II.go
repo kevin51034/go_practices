43ab03de54fbb5740af9082ac73561ed878259c3
陳文擎 <kevin51034@gmail.com>
2021-04-24 22:57:59 +0800

    2021/04/24

--- 45. Jump Game II.go
+++ 45. Jump Game II.go
@@ -1,30 +1,30 @@
-// using top-down dp
+// top-down dp
 // Time complexity: O(n^2)
 // Space complexity: O(n)
 
 func jump(nums []int) int {
-    if len(nums) <= 1 {
-        return 0
-    }
-    f := make([]int, len(nums))
+	if len(nums) <= 1 {
+		return 0
+	}
+	f := make([]int, len(nums))
 
-    for i:=0; i<len(nums); i++ {
-        for j:=1; j<=nums[i] && i+j<len(nums); j++ {
-            if f[i+j] == 0 {
-                f[i+j] = f[i]+1
-                continue
-            }
-            f[i+j] = min(f[i+j], f[i]+1)
-        }
-    }
-    return f[len(nums)-1]
+	for i := 0; i < len(nums); i++ {
+		for j := 1; j <= nums[i] && i+j < len(nums); j++ {
+			if f[i+j] == 0 {
+				f[i+j] = f[i] + 1
+				continue
+			}
+			f[i+j] = min(f[i+j], f[i]+1)
+		}
+	}
+	return f[len(nums)-1]
 }
 
 func min(x, y int) int {
-    if x<y {
-        return x
-    }
-    return y
+	if x < y {
+		return x
+	}
+	return y
 }
 
 // greedy
@@ -32,25 +32,25 @@ func min(x, y int) int {
 // Space complexity: O(1)
 
 func jump(nums []int) int {
-    if len(nums) <= 1 {
-        return 0
-    }
-    curMax := 0
-    curEnd := 0
-    result := 0
-    for i:=0; i<len(nums); i++ {
-        if i > curEnd {
-            result ++
-            curEnd = curMax
-        }
-        curMax = max(curMax, i+nums[i])
-    }
-    return result
+	if len(nums) <= 1 {
+		return 0
+	}
+	curMax := 0
+	curEnd := 0
+	result := 0
+	for i := 0; i < len(nums); i++ {
+		if i > curEnd {
+			result++
+			curEnd = curMax
+		}
+		curMax = max(curMax, i+nums[i])
+	}
+	return result
 }
 
 func max(x, y int) int {
-    if x>y {
-        return x
-    }
-    return y
+	if x > y {
+		return x
+	}
+	return y
 }
